098bad0dd79e163c80f907755eccc844b20862b9
gtdany7 <analog.dfc@gmail.com>
2016-11-29 11:42:40 +0000

    Create puzzle.lisp

---
+++ Projecto Actual/29-11-2016/puzzle.lisp
@@ -0,0 +1,165 @@
+;;;; puzzle.lisp
+;;;; Disciplina de IA - 2016 / 2017
+;;;; Programador: Artur Esteves - 140221076
+;;;; Programador: Daniel Costa - 120221058
+;;;; Funções do domínio do problema
+
+;;; Tabuleiros Testes
+(defun tabuleiro-teste ()
+"retorna um tabuleiro vazio de dimensão 3 x 3"
+	(list '((NIL T T) (NIL NIL NIL) (NIL NIL NIL) (NIL NIL NIL))
+		  '((NIL NIL NIL) (T T T) (NIL NIL NIL) (NIL T NIL))
+	)
+)
+
+(defun tabuleiro-teste2 ()
+"retorna um tabuleiro vazio de dimensão 3 x 3"
+	(list '((T T T) (T T NIL) (NIL NIL NIL) (NIL NIL NIL))
+		  '((T T NIL) (NIL T NIL) (NIL NIL NIL) (NIL T NIL))
+	)
+)
+
+
+(defun no-teste ()
+"Tabuleiro=tabuleiro-teste ; profundidade=0; heuristica=0; pai=nil"
+	(list (tabuleiro-teste) 0 0 nil)
+)
+
+
+;;; Métodos de Consulta
+
+;; get-arcos-horizontais
+;; Teste: (get-arcos-horizontais (tabuleiro-teste1))   ->   ((NIL T T) (NIL NIL NIL) (NIL NIL NIL) (NIL NIL NIL)) 
+(defun get-arcos-horizontais (tabuleiro) "Retorna a lista dos arcos horizontais de um tabuleiro"
+	(car tabuleiro)
+)
+
+;; get-arcos-verticais
+;; Teste: (get-arcos-verticais (tabuleiro-teste1))   ->   ((NIL NIL NIL) (T T T) (NIL NIL NIL) (NIL T NIL))
+(defun get-arcos-verticais (tabuleiro) "Retorna a lista dos arcos verticiais de um tabuleiro"
+	(car (cdr tabuleiro))
+)
+
+
+;;; Funcões auxiliares dos operadores
+
+;; inserir-arco-na-posicao
+;; Teste: (inserir-arco-na-posicao 2 '(T nil T T))   ->   (T T T T)
+(defun inserir-arco-na-posicao (indice lista) "Insere um arco (representado pelo valor [T]) no índice da lista recebida"
+	(cond
+		((null lista) nil)
+		((= indice 1) (cons T (cdr lista)))
+		(T (cons (car lista) (inserir-arco-na-posicao (- indice 1) (cdr lista))))
+	)
+)
+
+;; inserir-arco-na-posicao-aux
+;; Teste: (inserir-arco-na-posicao-aux 3 3 (get-arcos-verticais (tabuleiro-teste1)))   ->   ((NIL NIL NIL) (T T T) (NIL NIL T) (NIL T NIL))
+(defun inserir-arco-na-posicao-aux (linha coluna lista) "Insere um arco (representado pelo valor [T]) numa lista que representa o conjunto de arcos dum tabuleiro. A posição representada pela linha e a coluna de destino são valores inteiros passados como argumentos"
+	(cond
+		((null lista) nil)
+		((= linha 1) (cons (inserir-arco-na-posicao coluna (car lista)) (cdr lista)))
+		(T (cons (car lista) (inserir-arco-na-posicao-aux (- linha 1) coluna (cdr lista))))
+	)
+)
+
+
+
+;;; Operadores
+
+;; arco-vertical
+;; Teste: (inserir-arco-vertical 3 3 (tabuleiro-teste1))   ->   (((NIL T T) (NIL NIL NIL) (NIL NIL NIL) (NIL NIL NIL)) ((NIL NIL NIL) (T T T) (NIL NIL T) (NIL T NIL)))
+;; Teste: (inserir-arco-vertical 1 3 (tabuleiro-teste))	   -> (((NIL T T) (NIL NIL NIL) (NIL NIL NIL) (NIL NIL NIL)) ((NIL NIL T) (T T T) (NIL NIL NIL) (NIL T NIL)))
+;; Teste: (inserir-arco-vertical 2 3 (tabuleiro-teste))    -> NIL
+(defun inserir-arco-vertical (linha coluna tabuleiro) "Insere um arco vertical (representado pelo valor [T]) num tabuleiro passado como argumento"
+	(cond
+		((or (null tabuleiro) (not (possivel-adicionar-arco linha coluna (get-arcos-verticais tabuleiro)))) nil)
+		(T (cons (get-arcos-horizontais tabuleiro) (list (inserir-arco-na-posicao-aux linha coluna (get-arcos-verticais tabuleiro)))))
+	)
+)
+
+;; arco-horizontal
+;; Teste: (inserir-arco-horizontal 3 3 (tabuleiro-teste1))   ->   (((NIL T T) (NIL NIL NIL) (NIL NIL T) (NIL NIL NIL)) ((NIL NIL NIL) (T T T) (NIL NIL NIL) (NIL T NIL)))
+;; Teste: (inserir-arco-horizontal 1 3 (tabuleiro-teste))	-> NIL
+(defun inserir-arco-horizontal (linha coluna tabuleiro) "Insere um arco horizontal (representado pelo valor [T]) num tabuleiro passado como argumento"
+	(cond
+		((or (null tabuleiro) (not (possivel-adicionar-arco linha coluna (get-arcos-horizontais tabuleiro))) ) nil)
+		(T (cons (inserir-arco-na-posicao-aux linha coluna (get-arcos-horizontais tabuleiro)) (list (get-arcos-verticais tabuleiro))))
+	)
+)	
+
+
+
+
+
+
+;; possivel-adicionar-arco
+;; Teste: (possivel-adicionar-arco 1 1 (get-arcos-horizontais (tabuleiro-teste1)))   -> Resultado:   T
+;; Teste: (possivel-adicionar-arco 1 3 (get-arcos-horizontais (tabuleiro-teste1)))   -> Resultado:   NIL
+(defun possivel-adicionar-arco (linha coluna lista) "Recebe indices de linha e coluna e uma lista de arcos horizontais ou de arcos verticais e verifica se naquela posição o valor é [T], se for devolve [NIL], se for [NIL] devolve [T]"
+	(cond
+		((null lista) nil)
+		((= linha 1) (possivel-adicionar-arco-aux coluna (car lista)))
+		(T (possivel-adicionar-arco (- linha 1) coluna (cdr lista)))
+	)
+)
+
+
+;; possivel-adicionar-arco-aux 
+;; Teste: (possivel-adicionar-arco-aux 3 '(NIL T NIL NIL))   -> Resultado: T
+;;Teste:  (possivel-adicionar-arco-aux 2 '(NIL T NIL NIL))	 ->Resultado: NIL
+(defun possivel-adicionar-arco-aux (indice lista)
+	(cond
+		((null lista) nil)
+		((and (= indice 1) (eql (car lista) nil)) T)
+		(T (possivel-adicionar-arco-aux (- indice 1) (cdr lista)))
+	)
+)
+
+
+
+#||
+
+	numero-caixas-fechadas 
+||#
+(defun testeh (linhah tabuleiro)
+	(cond
+			((null tabuleiro)nil)
+			(
+	
+	)
+
+
+)
+
+
+
+
+
+
+
+
+
+;;; heuristicas
+
+;; heuristica1
+;; Teste: (heuristica (tabuleiro1) 2)   -> Resultado: 1
+(defun heuristica1 (tabuleiro numero-caixas-a-fechar) "Usada uma heurística que priveligia os tabuleiros com o maior número de caixas fechadas"
+	(- numero-caixas-a-fechar (numero-caixas-fechadas tabuleiro)  1)
+)
+
+
+;;;;;;;;;;;; Necessário definir esta 2ª heurística
+;;;;;;;; A definir ainda
+;;; Usar nº de arcos ligados a um nó?
+(defun heuristica2 (tabuleiro numero-caixas-a-fechar)
+	nil
+)
+
+
+#||
+		FALTA
+
+Verificar se é nó objectivo
+
+||#
